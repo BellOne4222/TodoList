a89aaf2caf0a9e6266b551a1f3e8c6edd6fbabd8
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2024-02-03 02:21:05 +0900

    # 1
    TodoList Project

--- src/main/java/com/example/TodoServerApplication.java
+++ src/main/java/com/example/TodoServerApplication.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 프로젝트실행
 @SpringBootApplication
 public class TodoServerApplication {
 
